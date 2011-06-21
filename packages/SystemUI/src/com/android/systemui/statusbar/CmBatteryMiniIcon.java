1825a20368a33d715d100a8a2333468f6d3173ea
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-06-21 13:30:39 +0200

    Revert "Fixed regression: fully charged battery shows as depleted."
    This reverts commit 311c5b404f5ab77679f3dd426ca431e202e9d188.

--- packages/SystemUI/src/com/android/systemui/statusbar/CmBatteryMiniIcon.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/CmBatteryMiniIcon.java
@@ -67,6 +67,9 @@ public class CmBatteryMiniIcon extends ImageView {
     private boolean mShowCmBattery = false;
 
     // used for animation
+    private long mLastMillis = 0;
+
+    // used for animation
     private int mCurrentFrame;
 
     private boolean mAttached;
@@ -197,9 +200,6 @@ public class CmBatteryMiniIcon extends ImageView {
                         updateAnimDuration();
                 } else {
                     stopTimer();
-                    // As it's charged set the full charged frame, else
-                    // it might be shown as empty when fully charged.
-                    mCurrentFrame = 10;
                 }
             }
         }
