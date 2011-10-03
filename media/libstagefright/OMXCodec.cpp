2fe909e3c668404aaf9de91212a0a7174e4735b1
Quarx2k <agent00791@gmail.com>
2011-10-03 23:51:17 +0700

    allow to encode WVGA with 720p encoder (for tests)

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -776,7 +776,7 @@ status_t OMXCodec::configureCodec(const sp<MetaData> &meta, uint32_t flags) {
             bool success = meta->findInt32(kKeyWidth, &width);
             success = success && meta->findInt32(kKeyHeight, &height);
             CHECK(success);
-            if (width*height > 407040) {
+            if (width*height >= 407040) {
                 // need OMX.TI.720P.Encoder if > 480x848
                 LOGE("OMX.TI.720P.Encoder is required.");
                 return ERROR_UNSUPPORTED;
